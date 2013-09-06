8bd5bf71d62950f9ef1dd81620bdc4d4d93f993b
JonZach <Zach@yourgreenpal.com>
2013-09-06 14:41:16 -0500

    changed to list item presentation

--- www/js/index.js
+++ www/js/index.js
@@ -16,9 +16,18 @@
  * specific language governing permissions and limitations
  * under the License.
  */
-$.ajax("https://api.github.com/legacy/repos/search/javascript").done(function(data) {
-     var i, repo;
-     $.each(data.repositories, function (i, repo) {
-        $("#allRepos").append("<p><a href='https://github.com/" + repo.username + "/" + repo.name + "'>" + repo.name + "</a><br>"+ repo.description + "&g\lt;/p>");
-     });
+$('#reposHome').bind('pageinit', function(event) {
+    loadRepos();
 });
+
+function loadRepos() {
+    $.ajax("https://api.github.com/legacy/repos/search/javascript").done(function(data) {
+        var i, repo;
+        $.each(data.repositories, function (i, repo) {
+            $("#allRepos").append("<li><a href='https://github.com/" + repo.username + "/" + repo.name + "'>"
+            + "<h4>" + repo.name + "</h4>"
+            + "<p>" + repo.username + "</p></a></li>");
+        });
+        $('#allRepos').listview('refresh');
+    });
+}
